c30aada0ec449deb1974a07061990a43e6a459cf
Brady Bridges <bradyjbridges@gmail.com>
2019-11-06 10:05:48 -0700

    Add apiCall to add order, implement adding order when name and one ingredient exists on submit

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -2,3 +2,22 @@ export const getOrders = () => {
   return fetch('http://localhost:3001/api/v1/orders')
       .then(response => response.json())
 }
+
+export const addOrder = async (name, ingredients) => {
+  const options = {
+    method: 'POST',
+    body: JSON.stringify({
+      name,
+      ingredients,
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  }; 
+  
+  const result = await fetch('http://localhost:3001/api/v1/orders', options);
+  console.log(result);
+  return result;
+}
+
+
